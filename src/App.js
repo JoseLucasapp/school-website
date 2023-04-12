edbb96d8c34057dbe8d3a8d1125cc81241469262
José Lucas <josluc2016@gmail.com>
2023-04-12 19:08:41 -0300

    feat: responsive itens page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import Home from './pages/home';
 import Item from './pages/item';
-import Login from './pages/login';
-import Unauthorized from './pages/unauthorized';
 
 function App() {
   return (
